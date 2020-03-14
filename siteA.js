3c7485d4a62bbdbfbd73471588bfdf06704afdb2
deewooley <55896326+deewooley@users.noreply.github.com>
2020-03-14 17:09:23 -0400

    practice with js arrow functions

---
+++ siteA.js
@@ -0,0 +1,11 @@
+'use strict'
+/*
+function printUserName(username){
+	console.log(username)
+}
+printUserName('Dee');
+*/
+let printFullName = (firstName, lastName) => {
+	console.log(firstName +" "+ lastName);
+}
+printFullName('Mike', 'Jones');
